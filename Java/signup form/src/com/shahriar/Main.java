339ab8906b7bc897634ed88c2b52f64a2d75419f
shahriaarrr <75499598+shahriaarrr@users.noreply.github.com>
2021-03-31 21:47:27 +0430

    add signup form project.

---
+++ Java/signup form/src/com/shahriar/Main.java
@@ -0,0 +1,11 @@
+package com.shahriar;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Signup s = new Signup();
+
+	    s.setupAndShowFrame();
+	    s.setAndShowButton();
+    }
+}
